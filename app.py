0419cd01cd286d8e6c7ac6492df0c71a7dbc8f67
43234sddwda <96641210+43234sddwda@users.noreply.github.com>
2023-03-29 19:55:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+   return render_template('modal.html')
 
 @app.route('/modal', methods=['POST'])
 def modal_post():
