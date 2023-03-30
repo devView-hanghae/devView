44fa7e562009c5f266ef88892547ade4fd276e8f
hansololiviakim <dev.hansolkim@gmail.com>
2023-03-30 13:16:24 +0900

    Feat: app.py 루트 추가

--- app.py
+++ app.py
@@ -6,15 +6,48 @@ with open("config.json", "r", encoding="utf-8") as f:
 # pymongo
 from pymongo import MongoClient
 client = MongoClient(config["dbUrl"])
-db = client.dbView
+# db = client.dbView
+db = client.dbTest
 
 # flask
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
+# 메인화면
 @app.route('/')
 def home():
-   return render_template('index.html')
+  return render_template('index.html')
+
+# 상세페이지
+@app.route('/page')
+def page():
+  return render_template('page.html')
+
+# 상세페이지 - 리스트 조회
+@app.route("/modal", methods=["GET"])
+def value_get():
+  test_data = list(db.value.find({}, {'_id': False}))
+  return jsonify({'result': test_data})
+
+# 상세페이지 - 키워드 검색
+@app.route("/search", methods=["POST"])
+def search():
+  # POST 요청으로부터 검색어를 가져옴
+  keyword = request.json['keyword']
+  # MongoDB에서 검색
+  result = db.value.find({'videoname': {'$regex': keyword}})
+  # 검색 결과를 리스트로 변환
+  data = []
+  for doc in result:
+    data.append({
+      'videoname': doc['videoname'],
+      'videodesc': doc['videodesc'],
+      'videolink': doc['videolink'],
+    })
+  # 검색 결과를 JSON 형태로 반환
+  return jsonify(data)
+
+
 
 # flask port
 if __name__ == '__main__':  
